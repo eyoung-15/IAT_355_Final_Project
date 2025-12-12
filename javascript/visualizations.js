0785cd50fe2f7023dee47eb132a7aa094e4bc5e6
Emily Youngward <eyoungwa@gmail.com>
2025-12-12 15:05:45 -0700

    adding new dataset

--- javascript/visualizations.js
+++ javascript/visualizations.js
@@ -3392,7 +3392,7 @@ async function drawGreedyArtistsChart() {
             price: +d.Average_Ticket_Price,
             isGuess: false
         }))
-        .filter(d => d.price >= 148.5 && d.price <= 300)
+        .filter(d => d.price >= 169.88 && d.price <= 300)
         .sort((a, b) => b.price - a.price);
 
     let guessData = null;
